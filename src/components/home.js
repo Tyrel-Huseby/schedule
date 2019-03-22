38e96d01a2c690fa71f70f5b0ef7bcd0fb4283d0
Tyrel Huseby <thuseby.business@gmail.com>
2019-03-22 12:56:52 -0600

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Course Schedule</h1>
+        <h2>React Redux</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
